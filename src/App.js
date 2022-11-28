bf187e31d7ac38d2d27b2ea1725a0b8031be526d
crimmzx <111248873+crimmzx@users.noreply.github.com>
2022-11-27 19:24:18 -0500

    second attempt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import timelineElements from './timelineElements';
+import {FaUserFriends} from 'react-icons/fa';
+import { ReactComponent as SchoolIcon } from "./school.svg";
+
+
+import{VerticalTimeline, 
+  VerticalTimelineElement
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css"
 
 function App() {
+  let workIconStyles = { background: "#06D6A0" };
+  let schoolIconStyles = { background: "#f9c74f" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1 className = "title">BePrinceton.</h1>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"></link>
+    <VerticalTimeline>
+      {
+        timelineElements.map(element => {
+          let isWorkIcon = element.icon === "work"
+          let showButton = element.buttonText !== undefined && 
+          element.buttonText !== null && 
+          element.buttonText !== "";
+
+          return(
+            <VerticalTimelineElement
+            key = {element.key}
+            date = {element.date}
+            dateClassName = "date"
+            iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+            icon = {isWorkIcon ? <FaUserFriends /> : <SchoolIcon />}
+            >
+            <h3 className='vertical-timeline-element-title'>
+                {element.title}
+            </h3>
+            <img src = {element.image} alt = "big boss" width = "200" height = "300"></img>
+            <h5 className='vertical-timeline-element-subtitle'>
+                {element.location}
+            </h5>
+            <p id = "description">{element.description}</p>
+            {showButton && (<a className = {`button ${
+              isWorkIcon ? "workButton" : "schoolBUtton"}`}
+              href ="/"
+              >
+              {element.buttonText}
+              </a>
+              )}
+            </VerticalTimelineElement>
+          );
+        })}
+    </VerticalTimeline>
+  </div>
   );
 }
 
